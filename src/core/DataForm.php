651eb39babf900b316b21114bc161984cc9e7d61
olapawelec13@gmail.com <olapawelec13@gmail.com>
2021-05-29 00:00:10 +0200

    add data form base

---
+++ src/core/DataForm.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Core;
+
+use Database;
+
+class DataForm
+{
+    public function __construct($classname, $id = null) {
+
+    }
+
+    public function displayForm() {
+
+    }
+
+    public function create() {
+        
+    }
+
+    public function insert() {
+
+    }
+
+    public function update() {
+
+    }
+
+    public function delete() {
+
+    }
+}
